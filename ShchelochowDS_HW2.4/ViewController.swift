05b9ea5b16ad2b0fd4ff5324c8180a0e7fe8b024
Дмитрий Щелочков <ddimpalass@icloud.com>
2020-12-11 10:00:43 +0300

    Initial Commit

---
+++ ShchelochowDS_HW2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShchelochowDS_HW2.4
+//
+//  Created by Apple on 11.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
